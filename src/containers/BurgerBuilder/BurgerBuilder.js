bf81163aa3a068f3f29c6552292e29800fd8bc80
Rachita Upreti <rachita.upreti@gmail.com>
2019-04-30 10:02:35 +0300

    reducer for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,17 +27,17 @@ class BurgerBuilder  extends Component{
 
     componentDidMount (){
         console.log("check",this.props)
-        axios.get('https://burgerapp-20fa8.firebaseio.com/ingredients.json')
-        .then(response =>{
-            this.setState({
-                ingredients:response.data
-            });
-        })
-        .catch(error => {
-            this.setState({
-                error:true
-            })
-        })
+        // axios.get('https://burgerapp-20fa8.firebaseio.com/ingredients.json')
+        // .then(response =>{
+        //     this.setState({
+        //         ingredients:response.data
+        //     });
+        // })
+        // .catch(error => {
+        //     this.setState({
+        //         error:true
+        //     })
+        // })
     }
     updatePurchaseState (ingredients) {
         const sum = Object.keys(ingredients)
